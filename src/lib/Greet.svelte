a1250108cd36ef6d351b5f3aaa033ad3e95029b2
FedericoCantarelli <79110279+FedericoCantarelli@users.noreply.github.com>
2024-07-31 22:12:04 +0200

    first commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { invoke } from '@tauri-apps/api/tauri'
+
+  let name = ''
+  let greetMsg = ''
+
+  async function greet() {
+    greetMsg = await invoke('greet', { name })
+  }
+</script>
+
+<div>
+  <input id="greet-input" placeholder="Inserisci un animale..." bind:value="{name}" />
+  <button on:click="{greet}">Genera</button>
+  <p>{greetMsg}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Greet from '../lib/Greet.svelte';
+</script>
+
+<h1>Benvenuto al generatore di porchi 🐷</h1>
+<Greet />
